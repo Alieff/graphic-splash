be2db1cd235f8a2ab03c5d1e9fe566e683aaf34b
Alief <serdralfs@gmail.com>
2019-02-05 22:20:36 +0700

    update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ mkdir -p "$HOME/Documents/.script/misc"
 installer_path="$(dirname ${BASH_SOURCE[0]})"
 destination_path="$HOME/Documents/.script/misc"
 script_name='splash.py'
-script_identifier='alieff/splash'
+script_identifier='alieff/graphic-splash'
+alias_name='splash'
 
 # copy the script 
 if [[ -z "$(ls $destination_path | grep $script_name)" ]]; then 
@@ -17,14 +18,15 @@ else # case if already installed
 fi
 
 # register the script for auto source
-if [[ -z "$(cat $HOME/.bashrc | grep $script_identifier)" ]]; then 
+if [ -z "$(cat $HOME/.bashrc | grep $script_identifier)" ] && [ $(alias "$alias_name"; echo $?) -eq 1 ]; then 
   # add identifier
   echo "# $script_identifier" >> $HOME/.bashrc
   # add auto source script
-  "echo" "alias splash='python $destination_path/$script_name'" >> $HOME/.bashrc
+  "echo" "alias $alias_name='python $destination_path/$script_name'" >> $HOME/.bashrc
   echo "registering to bashrc done"
+  echo "if you want to remove the alias just edit '$HOME/.bashrc'"
 else # case if already installed
   echo 
-  echo "$script_name auto source is already defined"
+  echo "$script_name auto source is already defined / alias already defined"
   echo
 fi
