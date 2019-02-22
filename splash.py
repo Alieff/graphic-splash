7646f12a947df89a0b5af87849a2104d0fca6d4f
Alief <serdralfs@gmail.com>
2019-02-23 06:32:14 +0700

    update to using python 3, and gtk 3

--- splash.py
+++ splash.py
@@ -9,140 +9,81 @@ Note: this is a proof of concept file. It works, but it is by no means productio
 
 import sys
 import os
-import gobject
-import pango
-import pygtk
-import gtk
-from gtk import gdk
+import gi
+gi.require_version('Gtk','3.0')
+from gi.repository import Gtk, Gdk, GObject
 import cairo
 import gobject
+class TransparentWindow(Gtk.Window):
+    def __init__(self, filename):
+        Gtk.Window.__init__(self)
 
-class pngtranswin:
-	def __init__(self,filename):
-		self.filename = filename;
-		self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.set_size_request(300, 220)
 
-		self.window.set_decorated(0) ### agar border window terlihat/tidak terlihat (0=hidden/1=shown), origin=0
-		self.window.set_default_size(800, 650)
-
-		self.window.set_events(gtk.gdk.ALL_EVENTS_MASK)
-
-		# Initialize colors, alpha transparency		
-		self.window.set_app_paintable(1) ### agar window bisa diwarnain (0=ga bisa / 1=bisa), origiin=1
-		self.gtk_screen = self.window.get_screen()
-		colormap = self.gtk_screen.get_rgba_colormap()
-		if colormap == None:
-			colormap = self.gtk_screen.get_rgb_colormap()
-		gtk.widget_set_default_colormap(colormap)
-		if not self.window.is_composited():
-			self.supports_alpha = False
-		else:
-			self.supports_alpha = True
-
-		self.w,self.h = self.window.get_size()
-
-		self.window.connect("expose_event", self.expose)
-		self.window.connect("destroy", gtk.main_quit)
+        # pasang event delete, biar bisa di close
+        self.connect('destroy', Gtk.main_quit)
+        ### agar border window terlihat/tidak terlihat (0=hidden/1=shown), origin=0
+        self.set_decorated(0)
+        self.move(0,0)
 
 
-	def expose (self, widget, event):
-		self.ctx = self.window.window.cairo_create()
-		# set a clip region for the expose event, XShape stuff
-		self.ctx.save()
-		if self.supports_alpha == False:
-			self.ctx.set_source_rgb(1, 1, 1)
-		else:
-			self.ctx.set_source_rgba(1, 1, 1,0) ### biar background window transparan
-		self.ctx.set_operator (cairo.OPERATOR_SOURCE)
-		self.ctx.paint()
-		self.ctx.restore()
-		self.ctx.rectangle(event.area.x, event.area.y,
-				event.area.width, event.area.height)
-		self.ctx.clip()
-		# self.draw_image(self.ctx,0,0,'base.png')   ### membuat gambar, bisa ditambahin juga koordinatnya
-		self.draw_image(self.ctx,0,0,self.filename)   ### membuat gambar, bisa ditambahin juga koordinatnya
+        # bikin transparan
+        screen = self.get_screen()
+        visual = screen.get_rgba_visual()
+        # make window transparent
+        if visual and screen.is_composited():
+            self.set_visual(visual)
 
-	def setup(self):
-		# Set menu background image
-		self.background =  gtk.Image()
-		self.frame = gtk.Fixed()
-		self.window.add (self.frame)
+        # define widget content
+        self.entry = Gtk.Entry()
+        # tambah gambar
+        image = Gtk.Image()
+        image.set_from_file(filename)
+        # dengarkan event
+        self.entry.connect('key_release_event', self.on_key_press)
 
-		# Set window shape from alpha mask of background image
-		w,h = self.window.get_size()
-		if w==0: w = 800
-		if h==0: h = 650
-		self.w = w
-		self.h = h
-		self.pixmap = gtk.gdk.Pixmap (None, w, h, 1)
-		ctx = self.pixmap.cairo_create()
-		# todo: modify picture source
-		#self.bgpb = gtk.gdk.pixbuf_new_from_file('base.png')
-		ctx.save()
-		ctx.set_source_rgba(1, 1, 1,0)
-		ctx.set_operator (cairo.OPERATOR_SOURCE)
-		ctx.paint()
-		ctx.restore()
-		# self.draw_image(ctx,0,0,'base.png')   ### membuat gambar, bisa ditambahin juga koordinatnya
-		self.draw_image(ctx,0,0,self.filename)   ### membuat gambar, bisa ditambahin juga koordinatnya
+        # tambah widget
+        box = Gtk.VBox()
+        entry_box = Gtk.HBox()
+        entry_box.show()
 
-		if self.window.is_composited():
-			ctx.rectangle(0,0,w,h)
-			ctx.fill()
-			self.window.input_shape_combine_mask(self.pixmap,0,0)
-		else:
-			self.window.shape_combine_mask(self.pixmap, 0, 0)
+        box.pack_start(image,  expand = False, fill = False, padding = 0)
+        box.pack_start(entry_box,  expand = False, fill = False, padding = 0)
+        entry_box.pack_start(self.entry,  expand = False, fill = False, padding = 0)
+        self.add(box)
 
-	def draw_image(self,ctx,x,y, pix):
-		"""Draws a picture from specified path with a certain width and
-height"""
+        self.set_app_paintable(True)
+        # display window
+        self.show_all()
 
-		ctx.save()
-		ctx.translate(x, y)
-		pixbuf = gtk.gdk.pixbuf_new_from_file(pix)
-		format = cairo.FORMAT_RGB24
-		if pixbuf.get_has_alpha():
-			format = cairo.FORMAT_ARGB32
-		#if Images.flip != None:
-		#	pixbuf = pixbuf.flip(Images.flip)
+    # get text on enter
+    def on_key_press(self, widget, event):
+        # print("      Key val, name: ", event.keyval, Gdk.keyval_name(event.keyval))
+        keypress = Gdk.keyval_name(event.keyval)
+        if keypress == 'Return':
+            print(self.entry.get_text())
+            self.destroy()
 
-		iw = pixbuf.get_width()
-		ih = pixbuf.get_height()
-		image = cairo.ImageSurface(format, iw, ih)
-		image = ctx.set_source_pixbuf(pixbuf, 0, 0)
-
-		ctx.paint()
-		puxbuf = None
-		image = None
-		ctx.restore()
-		ctx.clip()
-
-	def show_window(self):
-		self.window.show_all()
-		while gtk.events_pending():
-			gtk.main_iteration()
-		self.window.present()
-		self.window.grab_focus()
-		self.p = 1
 
 if __name__ == "__main__":
-	filename="/home/pulpen/Pictures/bot/hello-fusion.png"
-	if len(sys.argv) > 1:
-		if sys.argv[1] == "hello":
-			filename="/home/pulpen/Pictures/bot/hello-fusion.png"
-		elif sys.argv[1] == "abis":
-			filename="/home/pulpen/Pictures/bot/abis-fusion.png"
-		elif sys.argv[1] == "penuh":
-			filename="/home/pulpen/Pictures/bot/penuh-fusion.png"
-		elif sys.argv[1] == "pre_solat":
-			filename="/home/pulpen/Pictures/bot/pre-solat-fusion.png"
-		elif sys.argv[1] == "solat":
-			filename="/home/pulpen/Pictures/bot/solat-fusion.png"
-		elif sys.argv[1] == "ingetin_minum":
-			filename="/home/pulpen/Pictures/bot/minum-fusion.png"
-                else :
-                    filename=sys.argv[1]
-	m = pngtranswin(filename)
-	m.setup()
-	m.show_window()
-	gtk.main()
+    filename="/home/pulpen/Pictures/bot/hello-fusion.png"
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "hello":
+            filename="/home/pulpen/Pictures/bot/hello-fusion.png"
+        elif sys.argv[1] == "abis":
+            filename="/home/pulpen/Pictures/bot/abis-fusion.png"
+        elif sys.argv[1] == "penuh":
+            filename="/home/pulpen/Pictures/bot/penuh-fusion.png"
+        elif sys.argv[1] == "pre_solat":
+            filename="/home/pulpen/Pictures/bot/pre-solat-fusion.png"
+        elif sys.argv[1] == "solat":
+            filename="/home/pulpen/Pictures/bot/solat-fusion.png"
+        elif sys.argv[1] == "ingetin_minum":
+            filename="/home/pulpen/Pictures/bot/minum-fusion.png"
+        else :
+            filename=sys.argv[1]
+
+# spawn transparent window
+TransparentWindow(filename)
+# start processing loop biar ga langsung ke close
+Gtk.main()
